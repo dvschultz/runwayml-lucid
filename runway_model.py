02f24e687511776081c96b38ad8e22ea0e27ef25
Derrick Schultz <dvsmethid@gmail.com>
2020-11-09 23:43:47 -0500

    first attempt

--- runway_model.py
+++ runway_model.py
@@ -37,8 +37,14 @@
 # Import the Runway SDK. Please install it first with
 # `pip install runway-python`.
 import runway
-from runway.data_types import number, text, image
-from example_model import ExampleModel
+from runway.data_types import category, image
+import numpy as np
+import tensorflow as tf
+# from example_model import ExampleModel
+
+import lucid.modelzoo.vision_models as models
+from lucid.misc.io import show
+import lucid.optvis.render as render
 
 # Setup the model, initialize weights, set the configs of the model, etc.
 # Every model will have a different set of configurations and requirements.
@@ -46,29 +52,44 @@ from example_model import ExampleModel
 # supported configs. The setup function should return the model ready to be
 # used.
 setup_options = {
-    'truncation': number(min=1, max=10, step=1, default=5, description='Example input.'),
-    'seed': number(min=0, max=1000000, description='A seed used to initialize the model.')
+    "network": category(choices=["InceptionV1","VGG19_caffe"], default="InceptionV1")
 }
 @runway.setup(options=setup_options)
 def setup(opts):
-    msg = '[SETUP] Ran with options: seed = {}, truncation = {}'
-    print(msg.format(opts['seed'], opts['truncation']))
-    model = ExampleModel(opts)
+    msg = '[SETUP] Ran with options: network = {}'
+    print(msg.format(opts['network']))
+    if(opts['network'] == "InceptionV1"):
+        model = models.InceptionV1()
+    elif (opts['network'] == "VGG19_caffe"):
+        model = models.VGG19_caffe()
+    else:
+        model = models.InceptionV1()
+
+    model.load_graphdef()
     return model
 
 # Every model needs to have at least one command. Every command allows to send
 # inputs and process outputs. To see a complete list of supported inputs and
 # outputs data types: https://sdk.runwayml.com/en/latest/data_types.html
+
+input_options = {
+  'layer': category(choices=["conv2d0 (max:64)", "maxpool0 (max:64)", "conv2d1 (max:64)", "conv2d2 (max:192)", "maxpool1 (max:192)", "mixed3a (max:256)", "mixed3b (max:480)", "maxpool4 (max:480)", "mixed4a (max:508)", "mixed4b (max:512)", "mixed4c (max:512)", "mixed4d (max:528)", "mixed4e (max:832)", "maxpool10 (max:832)", "mixed5a (max:832)", "mixed5b (max:1024)"], default="mixed5b (max:1024)", description='choose layer of network to visualize'),
+  'neuron': number(default=0, min=0, max=1023, step=1, description='Neuron ID')
+}
+
 @runway.command(name='generate',
-                inputs={ 'caption': text() },
-                outputs={ 'image': image(width=512, height=512) },
-                description='Generates a red square when the input text input is "red".')
+                inputs={ input_options },
+                outputs={ 'image': image() },
+                description='Use Lucid to visualize the layers and neurons of a specific ML network.')
 def generate(model, args):
-    print('[GENERATE] Ran with caption value "{}"'.format(args['caption']))
+    print('[GENERATE] Ran with layer {} and neuron {}'.format(args['layer'], args['neuron']))
     # Generate a PIL or Numpy image based on the input caption, and return it
-    output_image = model.run_on_input(args['caption'])
+
+    layer_id = args['layer'].split(' ')[0]
+    layer_neuron = '{}:{}'.format(layer_id, args['neuron'])
+    img = render.render_vis(model, layer_neuron)
     return {
-        'image': output_image
+        'image': img
     }
 
 if __name__ == '__main__':
